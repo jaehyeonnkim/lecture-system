72b9f02e79a40806d28bed9e664936955bfcc75d
jaehyeonkim <wogus6805@gmail.com>
2024-12-26 20:27:02 +0900

    Initial commit

---
+++ src/main/java/org/lecture/lectureproject/LectureProjectApplication.java
@@ -0,0 +1,13 @@
+package org.lecture.lectureproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lecture/lectureproject/LectureProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lecture.lectureproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
